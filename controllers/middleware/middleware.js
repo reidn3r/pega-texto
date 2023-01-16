8fd329f219974fd2338a604fdcef41e3df05d9e5
reidn3r <reidnersteam@hotmail.com>
2023-01-16 18:25:31 -0300

    update: socket bug fixed

--- controllers/middleware/middleware.js
+++
@@ -1,5 +0,0 @@
-const get = (req, res, next) => {
-    const { id } = req.params;
-    console.log(id);
-    next();
-}
--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,10 @@
-//expressjs requirement
 const express = require('express');
 const router = express.Router();
 
-router.get('/', require('../controllers/getHome'));
+router.get('/', require('../controllers/renderViews').getHome);
 
-router.get('/:id', require('../controllers/getId'));
+router.get('/:id', require('../controllers/renderViews').getId);
 
-router.post('/', require('../controllers/postHome'));
+router.post('/', require('../controllers/renderViews').postHome);
 
 module.exports = router;
